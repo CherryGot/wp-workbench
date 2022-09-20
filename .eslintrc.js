31d3f8a557af4e18cb7387e58c7c83bda4c75edb
Chakrapani Gautam <cherrygot.dev@gmail.com>
2022-09-20 19:52:28 +0530

    Added linter for JS files

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'object-curly-spacing': [
+      'error',
+      'always',
+    ],
+    'padded-blocks': [
+      'error',
+      {
+        'classes': 'always',
+      },
+    ],
+    'max-len': [
+      'error',
+      {
+        'code': 120,
+      },
+    ],
+    'brace-style': [
+      'error',
+      'stroustrup',
+    ],
+  },
+};
