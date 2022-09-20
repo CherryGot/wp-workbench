ce9b8c5a2b01fa85dcef09eaa3cefb99c78f930c
Chakrapani Gautam <cherrygot.dev@gmail.com>
2022-09-20 15:46:43 +0530

    Project setup and Initial Commit
    * Wrote package.json and composer.json files to work with package managers
    * Wrote basic rules in editorconfig for defining tab size, spaces and
      newline rules
    * Added .gitignore to avoid tracking node_modules/ and vendor/
    * Wrote plugin header and a basic readme describing the project

---
+++ README.md
@@ -0,0 +1,3 @@
+# KRUTI. - A Next-Gen WordPress Admin Interface
+
+Want something new and refreshing from WordPress but are stuck with using its default admin theme? Introducing KRUTI, the next-generation WordPress admin interface created to be a beautiful and user-friendly alternative to your standard WordPress editing experience. Available for free on WordPress.org, it's probably one of the most comprehensive and modern admin themes available to date.
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * KRUTI. — A Next-Gen WordPress Admin Interface
+ *
+ * @package           cherrygot/kruti-admin
+ * @author            Chakrapani Gautam <contact@cherrygot.me>
+ *
+ * @wordpress-plugin
+ * Plugin Name:       KRUTI. — A Next-Gen WordPress Admin Interface
+ * Plugin URI:        https://cherrygot.me/projects/kruti-admin/
+ * Description:       Want something new and refreshing from WordPress but are stuck with using its default admin theme? Introducing KRUTI, the next-generation WordPress admin interface created to be a beautiful and user-friendly alternative to your standard WordPress editing experience. Available for free on WordPress.org, it's probably one of the most comprehensive and modern admin themes available to date.
+ * Version:           0.0.1
+ * Requires at least: 6.0
+ * Requires PHP:      8.0
+ * Author:            Chakrapani Gautam <contact@cherrygot.me>
+ * Author URI:        https://cherrygot.me/
+ * Text Domain:       kruti-admin
+ * License:           GPL v3
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Update URI:        https://cherrygot.me/projects/kruti-admin/
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+  die( 'Not today my friend, not today. Get back to what you were doing!' );
+}
