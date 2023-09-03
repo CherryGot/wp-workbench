ba9c2317be2aef2e128061dc7ecd40956340287e
Chakrapani Gautam <cherrygot.dev@gmail.com>
2023-09-03 21:26:45 +0530

    Fixed lint issues

--- workflow/readme-generator.php
+++ workflow/readme-generator.php
@@ -5,35 +5,34 @@
  *
  * @see https://github.com/fumikito/wp-readme/
  *
- * @package cherrygot/kruti-admin
+ * @package cherrygot/genz-admin
+ *
+ * phpcs:disable WordPress.WP.AlternativeFunctions
+ * phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
  */
 
+declare ( strict_types = 1 );
+
 /**
- * Wraps up logic for searching for readme and changlog files in a directory and compiling them to WordPress org
- * compatible readme file.
+ * Wraps up logic for searching for readme and changlog files in a directory and compiling them to
+ * WordPress org compatible readme file.
  */
 final class ReadmeGenerator {
 
   /**
-   * Stores the path of the directory where readme and changelog files have to be searched, and later readme.txt
-   * will have to be saved.
-   *
-   * @var string
+   * Stores the path of the directory where readme and changelog files have to be searched, and
+   * later readme.txt will have to be saved.
    */
   private string $target_dir;
 
   /**
    * Name of the file generated after compilation.
-   *
-   * @static
-   * @var string
    */
   private static string $target_file = 'readme.txt';
 
   /**
    * Possible names of the readme file.
    *
-   * @static
    * @var string[]
    */
   private static array $readme_names = array( 'readme.md', 'README.md' );
@@ -41,7 +40,6 @@ final class ReadmeGenerator {
   /**
    * Possible names of the changelog file.
    *
-   * @static
    * @var string[]
    */
   private static array $changelog_names = array( 'changelog.md', 'CHANGELOG.md' );
@@ -49,8 +47,8 @@ final class ReadmeGenerator {
   /**
    * Inititializes the ReadmeGenerator instance.
    *
-   * It takes an optional $target_dir argument that's later used to determine the location of the files.
-   * Here it is trimed to remove the ending / before further usage.
+   * It takes an optional $target_dir argument that's later used to determine the location of the
+   * files. Here it is trimed to remove the ending / before further usage.
    *
    * @param string $target_dir Path of the location where files are present.
    */
@@ -61,11 +59,11 @@ final class ReadmeGenerator {
   /**
    * Generates the readme.txt file.
    *
-   * It's a publicly exposed function that locates the readme and changelog in the target directory and generates
-   * the readme.txt file, while logging the status on the screen.
+   * It's a publicly exposed function that locates the readme and changelog in the target directory
+   * and generates the readme.txt file, while logging the status on the screen.
    */
-  public function generate() {
-    $readme_file = $this->find_readme();
+  public function generate(): void {
+    $readme_file    = $this->find_readme();
     $changelog_file = $this->find_changelog();
 
     echo 'Found README.md and CHANGELOG.md...' . PHP_EOL;
@@ -76,9 +74,10 @@ final class ReadmeGenerator {
   /**
    * Locates the Github compatible readme file.
    *
+   * @return string Path to readme file.
    * @throws Exception If the readme file is not found.
    */
-  private function find_readme() {
+  private function find_readme(): string {
     $path = $this->get_file_path( ...self::$readme_names );
     if ( empty( $path ) ) {
       throw new Exception( 'Can\'t find the README.md in the target directory.' );
@@ -90,9 +89,10 @@ final class ReadmeGenerator {
   /**
    * Locates the Github compatible changelog file.
    *
+   * @return string Path to ChangeLog file.
    * @throws Exception If the changelog file is not found.
    */
-  private function find_changelog() {
+  private function find_changelog(): string {
     $path = $this->get_file_path( ...self::$changelog_names );
     if ( empty( $path ) ) {
       throw new Exception( 'Can\'t find the CHANGELOG.md in the target directory.' );
@@ -104,20 +104,22 @@ final class ReadmeGenerator {
   /**
    * Determines the path of Github compatible files.
    *
-   * It scans the target directory for the files using the list of names passed as arguments. If the file
-   * with one of the said names is found, it returns its path.
+   * It scans the target directory for the files using the list of names passed as arguments. If
+   * the file with one of the said names is found, it returns its path.
    *
    * @param string ...$file_names List of file names to search.
-   * @return string Path to the one of the files from the list of file names passed.
+   * @return string Path to the one of the files from the list of file names.
    */
-  private function get_file_path( string ...$file_names ) {
+  private function get_file_path( string ...$file_names ): string {
     if ( is_dir( $this->target_dir ) ) {
       foreach ( scandir( $this->target_dir ) as $file ) {
-        if ( in_array( $file, $file_names ) ) {
+        if ( in_array( $file, $file_names, strict: true ) ) {
           return $this->target_dir . DIRECTORY_SEPARATOR . $file;
         }
       }
     }
+
+    return '';
   }
 
   /**
@@ -130,21 +132,22 @@ final class ReadmeGenerator {
    * @throws Exception If the target file, readme.txt, is not writable.
    * @throws Exception If fails to save the generated file.
    */
-  private function generate_readme( string $readme_file, string $changelog_file ) {
+  private function generate_readme( string $readme_file, string $changelog_file ): void {
     if ( ! is_writable( $this->target_dir ) ) {
-      throw new Exception( "Target directory ${$this->target_dir} is not writable." );
+      throw new Exception( "Target directory {$this->target_dir} is not writable." );
     }
 
     $target_file = $this->target_dir . DIRECTORY_SEPARATOR . self::$target_file;
     if ( file_exists( $target_file ) && ! is_writable( $target_file ) ) {
-      throw new Exception( "Can't open the ${$target_file} as it's not writable." );
+      throw new Exception( "Can't open the {$target_file} as it's not writable." );
     }
 
-    $readme_content = file_get_contents( $readme_file ) . PHP_EOL . file_get_contents( $changelog_file );
+    $readme_content  = file_get_contents( $readme_file ) . PHP_EOL;
+    $readme_content .= file_get_contents( $changelog_file );
     $this->transform_markdown( $readme_content ); // passed by reference.
 
     if ( ! file_put_contents( $target_file, $readme_content ) ) {
-      throw new Exception( "Failed to save the file: ${$target_file}." );
+      throw new Exception( "Failed to save the file: {$target_file}." );
     }
   }
 
@@ -153,7 +156,7 @@ final class ReadmeGenerator {
    *
    * @param string $markdown_text The text that needs to be modified. Passed as reference.
    */
-  private function transform_markdown( string &$markdown_text ) {
+  private function transform_markdown( string &$markdown_text ): void {
     // All the below functions take $markdown_text as pass by reference.
     $this->compile_visibility_blocks( $markdown_text );
     $this->transform_headings( $markdown_text );
@@ -161,20 +164,21 @@ final class ReadmeGenerator {
   }
 
   /**
-   * Filters and compiles the custom visibility blocks that let the content to either show only on Github
-   * or WordPress markdown files. Comes handy to keep content targetted for different platforms in one file.
+   * Filters and compiles the custom visibility blocks that let the content to either show only on
+   * Github or WordPress markdown files. Comes handy to keep content targetted for different
+   * platforms in one file.
    *
    * @param string $markdown_text The text that needs to be modified. Passed as reference.
    */
-  private function compile_visibility_blocks( string &$markdown_text ) {
+  private function compile_visibility_blocks( string &$markdown_text ): void {
     // Remove only:github visibility block.
-    $markdown_text = preg_replace( '#<!-- only:github/ -->(.*?)<!-- /only:github -->#us', '', $markdown_text );
+    $regex         = '#(\n){0,2}<!-- only:github/ -->(.*?)<!-- /only:github -->(\n)?#us';
+    $markdown_text = preg_replace( $regex, PHP_EOL, $markdown_text );
 
     // Display only:wp comment block.
-    $tidy_matched = function( $matches ) {
-      return trim( $matches[1] );
-    };
-    $markdown_text = preg_replace_callback( '#<!-- only:wp>(.*?)</only:wp -->#us', $tidy_matched, $markdown_text );
+    $regex         = '#<!-- only:wp>(.*?)</only:wp -->#us';
+    $tidy_matched  = fn( array $matches ) => trim( $matches[1] );
+    $markdown_text = preg_replace_callback( $regex, $tidy_matched, $markdown_text );
   }
 
   /**
@@ -182,19 +186,20 @@ final class ReadmeGenerator {
    *
    * @param string $markdown_text The text that needs to be modified. Passed as reference.
    */
-  private function transform_headings( string &$markdown_text ) {
-    $transform_matched = function( $match ) {
+  private function transform_headings( string &$markdown_text ): void {
+    $transform_matched = function ( $matches ) {
       $sep    = '';
-      $length = strlen( $match[1] );
+      $length = strlen( $matches[1] );
 
       for ( $i = 1, $l = 3 - ( $length - 1 ); $i <= $l; $i += 1 ) {
         $sep .= '=';
       }
 
-      return "{$sep} {$match[2]} {$sep}";
+      return "{$sep} {$matches[2]} {$sep}";
     };
 
-    $markdown_text = preg_replace_callback( '/^(#+)\s+(.*)/mu', $transform_matched, $markdown_text );
+    $regex         = '/^(#+)\s+(.*)/mu';
+    $markdown_text = preg_replace_callback( $regex, $transform_matched, $markdown_text );
   }
 
   /**
@@ -202,13 +207,15 @@ final class ReadmeGenerator {
    *
    * @param string $markdown_text The text that needs to be modified. Passed as reference.
    */
-  private function transform_code_blocks( string &$markdown_text ) {
-    $markdown_text = preg_replace( '/```([^\n`]*?)\n(.*?)\n```/us', '<pre>$2</pre>', $markdown_text );
+  private function transform_code_blocks( string &$markdown_text ): void {
+    $regex         = '/```([^\n`]*?)\n(.*?)\n```/us';
+    $markdown_text = preg_replace( $regex, '<pre>$2</pre>', $markdown_text );
   }
 
 }
 
 // This file is executed as main routine.
+// phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_debug_backtrace
 if ( ! debug_backtrace() ) {
   $generator = new ReadmeGenerator( getenv( 'WP_README_DIR' ) ?: '.' );
 
@@ -216,7 +223,8 @@ if ( ! debug_backtrace() ) {
     $generator->generate();
   }
   catch ( Exception $e ) {
-    echo '[ERROR]' . $e->getMessage() . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+    // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+    echo '[ERROR]' . $e->getMessage() . PHP_EOL;
     exit( 1 );
   }
 }
