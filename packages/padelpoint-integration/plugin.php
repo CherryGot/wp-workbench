13f686a692f39886b0cb882abf8e9c2f7ecc358a
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-09-08 18:34:48 +0530

    feat(padelpoint-integration): registered custom product types

--- packages/padelpoint-integration/plugin.php
+++ packages/padelpoint-integration/plugin.php
@@ -15,3 +15,21 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
+require 'vendor/autoload.php';
+
+use PadelPoint\Product\Types;
+
+$init_handler = function (): void {
+  add_filter( 'product_type_selector', array( Types\Article::class, 'register' ) );
+  add_filter( 'product_type_selector', array( Types\Set::class, 'register' ) );
+
+  add_filter( 'woocommerce_product_class', array( Types\Article::class, 'resolve_class' ), 10, 2 );
+  add_filter( 'woocommerce_product_class', array( Types\Set::class, 'resolve_class' ), 10, 2 );
+};
+add_action( 'init', $init_handler );
+
+$admin_footer_handler = function (): void {
+  Types\Article::enable_simple_fields();
+  Types\Set::enable_variable_fields();
+};
+add_action( 'admin_footer', $admin_footer_handler );
---
+++ packages/padelpoint-integration/src/Product/Types/Set.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * Contains core logic to work with the "Conjuntos" product type from the catalog.
+ *
+ * @package arpadel
+ */
+
+declare (strict_types = 1);
+
+namespace PadelPoint\Product\Types;
+
+/**
+ * Wraps the logic that filedoc was talking about.
+ */
+class Set extends \WC_Product_Variable {
+
+  public const SLUG = 'padelpoint-set';
+
+  /**
+   * The return value of this member function is later used to define new product type.
+   */
+  public function get_type(): string {
+    return static::SLUG;
+  }
+
+  /**
+   * Registers this custom product type to appear in the product type selector.
+   *
+   * @param array<string,string> $types The list of already registered product types.
+   * @return array<string,string> Updated types.
+   */
+  public static function register( array $types ): array {
+    $types[ static::SLUG ] = __( 'PadelPoint Set', 'padelpoint-integration' );
+    return $types;
+  }
+
+  /**
+   * Resolves the product class to use based on the product type slug.
+   *
+   * @param string $classname    The existing classname that maybe overriden in the function.
+   * @param string $product_type The slug for the type of product.
+   */
+  public static function resolve_class( string $classname, string $product_type ): string {
+    if ( static::SLUG === $product_type ) {
+      return static::class;
+    }
+
+    return $classname;
+  }
+
+  /**
+   * Since this product type is just an extension over Variable Product type, we need to make sure
+   * that the woocommerce metabox shows relevant fields for our custom product type.
+   */
+  public static function enable_variable_fields(): void {
+    $show = 'show_if_' . static::SLUG;
+    $hide = 'hide_if_' . static::SLUG;
+    ?>
+    <script>
+      jQuery(document).ready(function($) {
+        $('#woocommerce-product-data .show_if_variable').addClass(
+          '<?php echo esc_attr( $show ); ?>'
+        );
+        $('#woocommerce-product-data .hide_if_variable').addClass(
+          '<?php echo esc_attr( $hide ); ?>'
+        );
+      });
+    </script>
+    <?php
+  }
+
+}
