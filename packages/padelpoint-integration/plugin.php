da190cd0529e57beb65aefcc26e00bbbdad7fe93
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-09-08 18:32:50 +0530

    chore(padelpoint-integration): init plugin

---
+++ packages/padelpoint-integration/plugin.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: PadelPoint API Integration
+ * phpcs:ignore Generic.Files.LineLength.MaxExceeded
+ * Description: Integration with the PadelPoint API to import product catalogue, check their availability and insert orders.
+ * Version:     0.0.0
+ * Author:      Chakrapani Gautam [https://cherrygot.me], Wendy Alarcón Mego
+ *
+ * @package arpadel
+ */
+
+declare ( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+  exit;
+}
+
