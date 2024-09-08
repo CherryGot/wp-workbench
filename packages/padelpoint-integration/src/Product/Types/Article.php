5888f5a4c05aefa71152cfad774ba1f5a2ee32ef
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-09-09 00:34:43 +0530

    feat(padelpoint-integration): logic to import an article product type

--- packages/padelpoint-integration/src/Product/Types/Article.php
+++ packages/padelpoint-integration/src/Product/Types/Article.php
@@ -69,4 +69,83 @@ class Article extends \WC_Product_Simple {
     <?php
   }
 
+  /**
+   * Imports an articulo present in padelpoint API and populates necessary ACF fields.
+   *
+   * @param array<string,mixed> $articulo The raw article product from catalog.
+   */
+  public static function import( array $articulo ): void {
+    if ( $articulo['CODIGO'] <= 0 ) {
+      error_log( "Weird article with CODIGO set to 0: {$articulo['DESCRIPCION']}" );
+      return;
+    }
+
+    $post_id          = 0;
+    $existing_product = \get_posts(
+      array(
+        'post_type'   => 'product',
+        'meta_key'    => '_sku',
+        'meta_value'  => $articulo['CODIGO'],
+        'numberposts' => 1,
+        'fields'      => 'ids',
+      )
+    );
+
+    if ( $existing_product ) {
+      // Actualizar el producto existente.
+      $post_id = $existing_product[0];
+      wp_update_post(
+        array(
+          'ID'         => $post_id,
+          'post_title' => $articulo['DESCRIPCION'],
+        )
+      );
+      \update_post_meta( $post_id, '_regular_price', $articulo['PRECIO'] );
+      \update_post_meta( $post_id, '_price', $articulo['PRECIO'] );
+      \update_post_meta( $post_id, '_stock', $articulo['STOCK'] );
+      error_log( 'Producto actualizado: ' . $articulo['DESCRIPCION'] . ' (ID: ' . $post_id . ')' );
+    }
+    else {
+      // Crear nuevo producto.
+      $post_id = \wp_insert_post(
+        array(
+          'post_title'  => $articulo['DESCRIPCION'],
+          'post_type'   => 'product',
+          'post_status' => 'publish',
+          'meta_input'  => array(
+            '_regular_price' => $articulo['PRECIO'],
+            '_price'         => $articulo['PRECIO'],
+            '_stock'         => $articulo['STOCK'],
+            '_sku'           => $articulo['CODIGO'],
+          ),
+        )
+      );
+      \wp_set_object_terms( $post_id, static::SLUG, 'product_type' );
+      error_log( "Producto insertado: {$articulo['DESCRIPCION']} (ID: $post_id)" . PHP_EOL );
+    }
+
+    if ( $post_id <= 0 ) {
+      return;
+    }
+
+    wp_set_object_terms( $post_id, $articulo['CODIGOS_CATWEB'], 'product_cat' );
+
+    // Actualizar campos personalizados (ACF).
+    if ( ! empty( $articulo['IMAGENES'] ) ) {
+      for ( $i = 0; $i < 2; $i += 1 ) { // Currently, expecting only two images.
+        if ( isset( $articulo['IMAGENES'][ $i ] ) ) {
+          \update_field( 'imagen_' . ( $i + 1 ), $articulo['IMAGENES'][ $i ], $post_id );
+        }
+      }
+    }
+
+    $fields  = array( 'MARCA', 'FORMA', 'SUPERFICIE', 'JUGADOR', 'NUCLEO', 'NIVEL' );
+    $fields += array( 'TIPO_DE_JUEGO', 'DUREZA', 'PESO', 'TIPO_DE_SUELA' );
+    foreach ( $fields as $field ) {
+      if ( isset( $articulo[ $field ] ) ) {
+        \update_field( strtolower( $field ), $articulo[ $field ], $post_id );
+      }
+    }
+  }
+
 }
