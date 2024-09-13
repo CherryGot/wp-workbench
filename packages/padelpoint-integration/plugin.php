33398d9b595e03a668140fe135c392e51b89cf21
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-09-13 11:59:25 +0530

    chore(padelpoint-integration): check if woocommerce and acf are there

--- packages/padelpoint-integration/plugin.php
+++ packages/padelpoint-integration/plugin.php
@@ -1,10 +1,11 @@
 <?php
 /**
- * Plugin Name: PadelPoint API Integration
+ * Plugin Name:      PadelPoint API Integration
  * phpcs:ignore Generic.Files.LineLength.MaxExceeded
- * Description: Integration with the PadelPoint API to import product catalogue, check their availability and insert orders.
- * Version:     0.0.0
- * Author:      Chakrapani Gautam [https://cherrygot.me], Wendy Alarcón Mego
+ * Description:      Integration with the PadelPoint API to import product catalogue, check their availability and insert orders.
+ * Version:          0.0.0
+ * Author:           Chakrapani Gautam [https://cherrygot.me], Wendy Alarcón Mego
+ * Requires Plugins: woocommerce, advanced-custom-fields
  *
  * @package arpadel
  */
