191f57891c3c52672c04b0ef8f0bff103e5a5de7
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-09-15 19:22:36 +0530

    feat(padelpoint-integration): added an interface to make API requests

---
+++ packages/padelpoint-integration/src/API.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file here contains the actual logic to contact the PadelPoint API.
+ *
+ * @package arpadel
+ */
+
+declare ( strict_types = 1 );
+
+namespace PadelPoint;
+
+/**
+ * Class to wrap up all the functions.
+ */
+class API {
+
+  private const ENDPOINT = 'https://api.originalpadelpoint.com/WebServices';
+
+  /**
+   * Prepare Basic Authentication hash from the user and password fetched from DB.
+   */
+  private static function get_auth_hash(): string {
+    $user = \get_option( 'padelpoint-login', '' );
+    $pswd = \get_option( 'padelpoint-password', '' );
+    return base64_encode( "$user:$pswd" );
+  }
+
+  /**
+   * Make an authenticated request to a path on the endpoint.
+   *
+   * @param string              $path The path to access.
+   * @param array<string,mixed> $args The arguments needed to make the request.
+   * @return array<string,mixed> Response data from the request.
+   */
+  private static function make_request( string $path, array $args ): array {
+    $args['headers']['Authorization'] = 'Basic ' . static::get_auth_hash();
+    $response                         = \wp_remote_request( static::ENDPOINT . $path, $args );
+    if ( \is_wp_error( $response ) ) {
+      error_log( 'Error al conectar con la API: ' . $response->get_error_message() . PHP_EOL );
+      return array();
+    }
+
+    $body = \wp_remote_retrieve_body( $response );
+    $body = trim( $body, "\xEF\xBB\xBF" );
+    $data = json_decode( $body, true );
+
+    if ( json_last_error() !== JSON_ERROR_NONE ) {
+      error_log( 'Error al decodificar JSON: ' . json_last_error_msg() . PHP_EOL );
+      return array(
+        'error'   => true,
+        'message' => json_last_error_msg(),
+      );
+    }
+
+    if ( isset( $data['error'] ) && $data['error'] ) {
+      error_log( 'Error en la respuesta de la API: ' . $data['message'] . PHP_EOL );
+      return $data;
+    }
+
+    return $data;
+  }
+
+}
