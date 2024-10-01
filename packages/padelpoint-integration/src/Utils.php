ca71046174869935463bf9e49478ee51c404d0fc
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-10-01 23:27:53 +0530

    fix(padelpoint-integration): incorrect syntax while calling error_log

--- packages/padelpoint-integration/src/Utils.php
+++ packages/padelpoint-integration/src/Utils.php
@@ -22,7 +22,7 @@ class Utils {
   public static function log( string $message ): void {
     if ( defined( 'WP_DEBUG' ) && defined( 'WP_DEBUG_LOG' ) && WP_DEBUG && WP_DEBUG_LOG ) {
       // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_error_log
-      error_log( message: "[PadelPoint Integration]: $message" );
+      error_log( "[PadelPoint Integration]: $message" );
     }
   }
 
