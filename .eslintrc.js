e24807d8861425b94b21f045cd9c8229f209bc9c
Chakrapani Gautam <cherrygot.dev@gmail.com>
2024-10-13 13:41:34 +0530

    chore: updated eslint and stylelint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,22 +3,28 @@ module.exports = {
   env: {
     jest: true,
   },
+  plugins: [ 'align-assignments' ],
   extends: [ 'plugin:@wordpress/eslint-plugin/recommended-with-formatting' ],
   rules: {
-    'brace-style': [
-      'error',
-      'stroustrup',
-    ],
-    'space-in-parens': [
-      'error',
-      'always',
-    ],
-    'max-len': [
-      'error',
-      {
-        'code': 100,
-      }
-    ],
+    'brace-style': [ 'error', 'stroustrup' ],
+    'space-in-parens': [ 'error', 'always' ],
+    'max-len': [ 'error', {
+      'code': 100,
+    } ],
     'indent': [ 'error', 2 ],
+    'no-multi-spaces': [ 'error', {
+      exceptions: {
+        'VariableDeclarator': true,
+        'ImportDeclaration': true,
+        'Property': true,
+      }
+    } ],
+    'align-assignments/align-assignments': [ 'warn', {
+      'AssignmentExpression': true,
+      'VariableDeclarator': true,
+    } ],
+    'import/no-extraneous-dependencies': [ 'error', {
+      'packageDir': '.'
+    } ],
   },
 }
