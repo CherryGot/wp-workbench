977e978b623b8bc8b349cc55107223d3920c11f3
Chakrapani Gautam <cherrygot.dev@gmail.com>
2025-03-19 23:57:17 +0530

    chore: run the test command with a test DB service in docker

---
+++ build/run-tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if ! [ -n "$GITHUB_ACTIONS" ]; then
+  docker compose -f ./docker-compose.test.yml up -d
+
+  # Wait for the database to be ready
+  echo "Waiting for MySQL to be ready..."
+  until docker exec $(docker compose -f ./docker-compose.test.yml ps -q test_database) mysqladmin ping --silent; do
+    sleep 1
+  done
+fi
+
+cleanup() {
+  if ! [ -n "$GITHUB_ACTIONS" ]; then
+    docker compose -f ./docker-compose.test.yml down
+  fi
+}
+trap cleanup EXIT  # Run `cleanup` when the script exits
+
+# Run the tests now
+if [ -d "packages/$1" ] && [ -e "packages/$1/phpunit.xml" ]; then
+  ./vendor/bin/phpunit --colors=always --testdox -c "packages/$1/phpunit.xml"
+fi
